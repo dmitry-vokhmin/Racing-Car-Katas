3a885581aeef8af05da1a341fa94523f41e898ed
dmitryvokhmin <boxdima1@gmail.com>
2023-03-11 13:20:40 -0500

    Turn ticket

--- Python/TurnTicketDispenser/turn_ticket.py
+++ Python/TurnTicketDispenser/turn_ticket.py
@@ -1,18 +1,37 @@
-class TurnTicket(object):
-    def __init__(self, turnNumber):
-        self.turnNumber = turnNumber
-
-class TurnNumberSequence(object):
-    _turnNumber = -1
-
-    @staticmethod
-    def next_turn_number():
-        TurnNumberSequence._turnNumber += 1
-        return TurnNumberSequence._turnNumber
+from typing import Type, Protocol
 
 
-class TicketDispenser(object):
-    def getTurnTicket(self):
-        newTurnNumber = TurnNumberSequence.next_turn_number()
-        newTurnTicket = TurnTicket(newTurnNumber)
-        return newTurnTicket
+class Ticket(Protocol):
+    def __init__(self, number: int):
+        ...
+
+
+class TurnTicket:
+    def __init__(self, turn_number):
+        self.turn_number = turn_number
+
+
+class Sequence(Protocol):
+    @classmethod
+    def next_turn_number(cls) -> int:
+        ...
+
+
+class TurnNumberSequence:
+    _turn_number = -1
+
+    @classmethod
+    def next_turn_number(cls):
+        cls._turn_number += 1
+        return cls._turn_number
+
+
+class TicketDispenser:
+    def __init__(self, sequence: Type[Sequence], ticket: Type[Ticket]):
+        self._ticket = ticket
+        self._sequence = sequence
+
+    def get_turn_ticket(self):
+        new_turn_number = self._sequence.next_turn_number()
+        new_turn_ticket = self._ticket(new_turn_number)
+        return new_turn_ticket
